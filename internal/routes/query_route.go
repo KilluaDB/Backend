77cb2d262ab75330995991432959a2f6fc17c321
Hassan Azmy <hassanhussien13579@gmail.com>
2025-12-03 01:09:50 +0200

    Reformatting query route

--- internal/routes/query_route.go
+++ internal/routes/query_route.go
@@ -19,14 +19,6 @@ func (r *QueryRoutes) RegisterRoutes(router *gin.RouterGroup) {
 	query := router.Group("/projects/:project_id/query")
 	query.Use(middlewares.Authenticate)
 	{
-		//* Connection management endpoints
-		// query.POST("/connections", r.handler.CreateConnection)
-		// query.GET("/connections", r.handler.GetConnections)
-		// query.GET("/connections/:id", r.handler.GetConnection)
-		// query.PUT("/connections/:id", r.handler.UpdateConnection)
-		// query.DELETE("/connections/:id", r.handler.DeleteConnection)
-		// query.POST("/connections/test", r.handler.TestConnection)
-
 		// Query execution endpoints
 		query.POST("/execute", r.handler.ExecuteQuery)
 		query.GET("/history", r.handler.GetQueryHistory)
