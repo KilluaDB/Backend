dc0df89b47586d3c0f045ed44d706adade4d0efd
Hassan Azmy <hassanazmy13579@gmail.com>
2025-12-16 21:30:26 +0200

    Adding routes for google Oauth

--- internal/routes/auth.go
+++ internal/routes/auth.go
@@ -9,10 +9,14 @@ import (
 
 type AuthRoutes struct {
 	handler *handlers.AuthHandler
+	googleAuthHandler *handlers.GoogleAuthHandler
 }
 
-func NewAuthRoutes(hander *handlers.AuthHandler) *AuthRoutes {
-	return &AuthRoutes{handler: hander}
+func NewAuthRoutes(hander *handlers.AuthHandler, googleAuthHandler *handlers.GoogleAuthHandler) *AuthRoutes {
+	return &AuthRoutes{
+		handler: hander,
+		googleAuthHandler: googleAuthHandler,
+	}
 }
 
 func (r *AuthRoutes) RegisterRoutes(router *gin.RouterGroup) {
@@ -21,6 +25,8 @@ func (r *AuthRoutes) RegisterRoutes(router *gin.RouterGroup) {
 		// Public routes
 		auth.POST("/register", r.handler.Register)
 		auth.POST("/login", r.handler.Login)
+		auth.GET("/google/login", r.googleAuthHandler.Login)	// the one it’s serving the static files for the UI
+		auth.GET("/google/callback", r.googleAuthHandler.Callback)	// the callback path, when you are developing a website which needs an external OAuth technology, at the moment you sent the data you will got a response to a callback endpoint of your API
 
 		// Protected routes
 		protected := auth.Group("/")
