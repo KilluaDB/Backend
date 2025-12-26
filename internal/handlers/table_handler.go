605ec7a627aeaced5a9978df23bc50a886076512
Hassan Azmy <hassanazmy13579@gmail.com>
2025-12-26 21:14:14 +0200

    Delete table handler

--- internal/handlers/table_handler.go
+++ internal/handlers/table_handler.go
@@ -65,6 +65,50 @@ func (h *TableHandler) CreateTable(c *gin.Context) {
 	responses.Success(c, http.StatusOK, response, "Table created successfully")
 }
 
+func (h *TableHandler) DeleteTable(c *gin.Context) {
+	projectId := c.Param("id")
+	if projectId == "" {
+		responses.Fail(c, http.StatusBadRequest, nil, "Project id is required")
+		return
+	}
+
+	userId, exists := c.Get("userId")
+	if !exists {
+		responses.Fail(c, http.StatusUnauthorized, nil, "Unauthorized")
+		return
+	}
+
+	var req services.DeleteTableRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		responses.Fail(c, http.StatusBadRequest, err, "Invalid request body")
+		return
+	}
+
+	userUUID, err := h.toUUID(userId)
+	if err != nil {
+		responses.Fail(c, http.StatusUnauthorized, err, "Invalid user Id format")
+		return
+	}
+
+	projectUUID, err := uuid.Parse(projectId)
+	if err != nil {
+		responses.Fail(c, http.StatusBadRequest, err, "Invalid projectId format")
+		return
+	}
+
+	result, err := h.tableService.DeleteTable(&req, userUUID, projectUUID)
+	if err != nil {
+		responses.Fail(c, http.StatusBadRequest, err, "Cannot delete the given table")
+		return
+	}
+
+	response := gin.H {
+		"result": result,
+	}
+
+	responses.Success(c, http.StatusOK, response, "Table deleted successfully")
+}
+
 func (h *TableHandler) toUUID(userId any) (uuid.UUID, error) {
 	switch v := userId.(type) {
 		case uuid.UUID:
