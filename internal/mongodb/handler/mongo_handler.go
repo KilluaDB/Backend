0895c9405bda1777901e38da17c9f01d5b92cc73
Hassan Azmy <hassanazmy13579@gmail.com>
2026-06-04 19:52:00 +0300

    feat(server): register document routes and wire service to server entrypoint

--- internal/mongodb/handler/mongo_handler.go
+++ internal/mongodb/handler/mongo_handler.go
@@ -3,10 +3,12 @@ package handler
 // MongoHandler groups HTTP handlers for MongoDB project APIs.
 type MongoHandler struct {
 	Collection *CollectionHandler
+	Document   *DocumentHandler
 }
 
-func NewMongoHandler(collection *CollectionHandler) *MongoHandler {
+func NewMongoHandler(collection *CollectionHandler, document *DocumentHandler) *MongoHandler {
 	return &MongoHandler{
 		Collection: collection,
+		Document: 	document,
 	}
 }
