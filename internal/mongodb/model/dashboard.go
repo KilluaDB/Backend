0f8cb9475633160cdb5635023b986b033c0e7418
Hassan Azmy <hassanazmy13579@gmail.com>
2026-06-05 22:57:54 +0300

    MongoDB dashboard model

--- internal/mongodb/model/dashboard.go
+++ internal/mongodb/model/dashboard.go
@@ -1,14 +1,15 @@
 package model
 
+type OperationStats struct {
+	Inserts int64 `json:"inserts"`
+	Updates int64 `json:"updates"`
+	Deletes int64 `json:"deletes"`
+}
+
 type MongoDashboardMetrics struct {
-	Database        string  `json:"database"`
-	DBSizeBytes     int64   `json:"db_size_bytes"`
-	Collections     int64   `json:"collections"`
-	TotalDocuments  int64   `json:"total_documents"`
-	ActiveConns     int64   `json:"active_connections"`
-	AvailableConns  int64   `json:"available_connections"`
-	ActiveOps       int64   `json:"active_operations"`
-	TotalInserts    int64   `json:"total_inserts"`
-	TotalUpdates    int64   `json:"total_updates"`
-	TotalDeletes    int64   `json:"total_deletes"`
+	Database        string  			`json:"database"`
+	DBSizeBytes     int64   			`json:"db_size_bytes"`
+	Collections     int64   			`json:"collections"`
+	TotalDocuments  int64   			`json:"total_documents"`
+	Last30Days		 OperationStats	`json:"last_30_days"`
 }
